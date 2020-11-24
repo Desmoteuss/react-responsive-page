b209c7973f7023065dfa853c538d7588941fc428
desmoteuss <desmoteuss@gmail.com>
2020-11-24 11:25:28 +0100

    adding navibar

---
+++ src/components/Navbar.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Navbar() {
+    return (
+        <div>
+           <div className="navibar-container container ">
+               <Link to='/' className="navibar-logo" >LOGO</Link>
+           </div>
+        </div>
+    )
+}
+
+export default Navbar
