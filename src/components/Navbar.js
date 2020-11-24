3fd057ce91c2dc3b6f260a782a6f84df7205b924
desmoteuss <desmoteuss@gmail.com>
2020-11-24 20:54:21 +0100

    adding react routere and react icons

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,12 +1,17 @@
 import React from 'react'
+import {Link} from 'react-router-dom'
+import { FaBars, FaTimes } from 'react-icons/fa'
 
 function Navbar() {
     return (
-        <div>
+        <>
+        <div className="navbar">
            <div className="navibar-container container ">
                <Link to='/' className="navibar-logo" >LOGO</Link>
+               <div className="menu-icon"></div>
            </div>
         </div>
+        </>
     )
 }
 
