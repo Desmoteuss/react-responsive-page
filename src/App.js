310ec2691a215618cf37abce5de57158353a6bc4
desmoteuss <desmoteuss@gmail.com>
2020-11-25 21:54:01 +0100

    adding navbar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import './App.css';
+import Navbar from './components/Navbar';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
 function App() {
   return (
-    <div className="App">
-    <h1>teest</h1>  
-    </div>
+    <Router>
+      <Navbar />
+    </Router>
   );
 }
-
 export default App;
