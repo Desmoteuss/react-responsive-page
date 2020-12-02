d7dd2296782dcfa3cae1088a89af3d70a0a9575f
desmoteuss <desmoteuss@gmail.com>
2020-12-02 23:08:26 +0100

    adding hero segment

--- src/App.js
+++ src/App.js
@@ -2,11 +2,13 @@ import React from 'react';
 import './App.css';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import HeroSection from './components/pages/HeroSection';
 
 function App() {
   return (
     <Router>
       <Navbar />
+      <HeroSection />
     </Router>
   );
 }
