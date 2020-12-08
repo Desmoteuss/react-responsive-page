17aae977a7fe179a48e036cf8249398d22d71491
desmoteuss <desmoteuss@gmail.com>
2020-12-08 23:26:12 +0100

    adding pricing

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './App.css';
+import Home from './components/pages/Home/Home';
 import Navbar from './components/Navbar';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import Footer from './components/pages/Footer/Footer';
@@ -9,7 +10,9 @@ function App() {
   return (
     <Router>
       <Navbar />
-    
+      <Switch>
+        <Route path='/' exact component={Home} />
+      </Switch>
       <Footer />
     </Router>
   );
