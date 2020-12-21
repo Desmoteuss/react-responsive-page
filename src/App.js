8275962676dd20a6b23bd1e1fe108fef4793bc46
desmoteuss <desmoteuss@gmail.com>
2020-12-21 20:37:48 +0100

    update on App.js

--- src/App.js
+++ src/App.js
@@ -1,20 +1,27 @@
 import React from 'react';
 import './App.css';
 import Home from './components/pages/Home/Home';
-import Navbar from './components/Navbar';
+import Services from './components/pages/Services/Services';
+import Products from './components/pages/Products/Products';
+import SignUp from './components/pages/SignUp/SignUp';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
 import Footer from './components/pages/Footer/Footer';
 
 
+
 function App() {
-  return (
-    <Router>
-      <Navbar />
-      <Switch>
-        <Route path='/' exact component={Home} />
-      </Switch>
-      <Footer />
-    </Router>
-  );
+return (
+<Router>
+<Navbar />
+<Switch>
+<Route path='/' exact component={Home} />
+<Route path='/services' component={Services} />
+<Route path='/products' component={Products} />
+<Route path='/sign-up' component={SignUp} />
+</Switch>
+<Footer />
+</Router>
+);
 }
 export default App;
